9782c8f4eaeb23ca3d3d382a076a07e3c6cb63e9
Ernesto Alvarez <alvarez93lopez@gmail.com>
2022-07-20 11:23:47 -0400

    hotfix. Mostrar fieldToSee en el Browse

--- publishable/config/dvoyager.php
+++ publishable/config/dvoyager.php
@@ -1,5 +1,38 @@
 <?php
-
+/*
+    Opciones para los breads
+    'nombreEnCamelCaseDelRecurso' => [
+        'type' => Tipo del campo en la Base de Datos - string,
+        'slugFrom' => Campo a partir se forma el slug. Tiene que existir el campo. - string
+        'single_name' => Nombre en singular que se muestra en los BREADs - string,
+        'plural_name' => Nombre en plural que se muestra en los BREADs - string,
+        'maxRecords' => total de elementos máximos a guardar. Si no se incluye se coge -1. - integer
+        'info' => [ \\Opcional
+            'idioma' => [
+                'nombre_del_campo' => Texto que se quiera mostrar. - string
+            ]
+        ],
+        'fields' => [
+            'nombre_en_snake_case_del_campo' => [
+                'type' => tipo de la base de datos. Si es una relación se pone tipo 'relation' - string,
+                'voyager_type' => Tipo de campo de voyager. Si no se introduce coge text - string,
+                'displayName' => nombre que se muestra del campo en los BREADs. - string,
+                'isNullable' => Si es requerido - boolean,
+                'isUnique' => Si es único - boolean,
+                'validation' => [
+                    'rule' => validaciones en formato: 'required|max:20' - string
+                ],
+                'relationType' => Solo se utiliza si el type es relation. Tipo de la relación. Actualmente solo belongsTo. - string,
+                'referenceField' => Solo se utiliza si el type es relation. Campo por el que se va a realizar la relación. Normalmente id. - string,
+                'relationModel' => Solo se utiliza si el type es relation. 
+                                   Namespace del Modelo al se apunta en la relación. 
+                                   Ej: App\Models\User. Si se utiliza un modelo que es generado por esta vía se puede poner solo el nombre del recurso.
+                                   Si se utiliza de la última forma, tiene que haberse generado con antelación. - string,
+                'fieldToSee' => Solo se utiliza si el type es relation. Campo perteneciente a la otra entidad por el que se va a ver. Si no se incluye se coge id. - string
+            ]
+        ]
+    ]
+*/
 return [
     'act_date_format' => 'd.m.Y',
     'breads' => [
--- views/bread/browse.blade.php
+++ views/bread/browse.blade.php
@@ -125,6 +125,8 @@
                                                     @include($row->details->view, ['row' => $row, 'dataType' => $dataType, 'dataTypeContent' => $dataTypeContent, 'content' => $data->{$row->field}, 'action' => 'browse', 'view' => 'browse', 'options' => $row->details])
                                                 @elseif($row->type == 'image')
                                                     <img src="@if( !filter_var($data->{$row->field}, FILTER_VALIDATE_URL)){{ Voyager::image( $data->{$row->field} ) }}@else{{ $data->{$row->field} }}@endif" style="width:100px">
+                                                @elseif($row->type == 'select_dropdown' && property_exists($row->details, 'relationship'))
+                                                    @include('dvoyager::formfields.relationship', ['view' => 'browse','options' => $row->details])
                                                 @elseif($row->type == 'relationship')
                                                     @include('voyager::formfields.relationship', ['view' => 'browse','options' => $row->details])
                                                 @elseif($row->type == 'select_multiple')
